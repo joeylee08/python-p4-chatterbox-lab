6f51daee42eb79d4b443cade62bc6a0bfba8c60d
Joseph <josephylee72@gmail.com>
2023-11-15 15:06:24 -0800

    completed chatterbox lab

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
+import ipdb
 from models import db, Message
 
 app = Flask(__name__)
@@ -14,13 +14,34 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
-
-@app.route('/messages/<int:id>')
+    if request.method == "GET":
+        messages = []
+        # messages = [item.to_dict() for item in Message.query.order_by('created_at').all()]
+        for item in Message.query.order_by('created_at').all():
+            messages.append(item.to_dict())
+        return make_response(messages, 200)
+    else:
+        new_message = Message(body=request.get_json()["body"], username=request.get_json()["username"])
+        db.session.add(new_message)
+        db.session.commit()
+        response_dict = new_message.to_dict()
+        return make_response(response_dict, 201)
+    
+@app.route('/messages/<int:id>', methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if request.method == "PATCH":
+        for attr in request.get_json():
+            setattr(message, attr, request.get_json()[attr])
+        db.session.add(message)
+        db.session.commit()
+        response_dict = message.to_dict()
+
+        return make_response(response_dict, 200)
+    else:
+        db.session.delete(message)
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, func
 from sqlalchemy_serializer import SerializerMixin
+from flask import Flask
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,3 +13,7 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
